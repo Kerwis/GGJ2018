99316385e225e96d05bd7e43d24b892cce9c5432
MiłoszB <milb1994@gmail.com>
2018-01-27 22:35:11 +0100

    Player instantiation

---
+++ Assets/[Scripts]/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	
+	public float _rotationFactor = 1f;
+	
+	[SerializeField]
+	private GameObject _mainCameraCenter;
+	[SerializeField] 
+	private GameObject _aimPoint;
+	
+	private float _rotationSpeedX = 1f;
+	private float _rotationSpeedY = 1f;
+	
+	private Vector3 _centerCurrentRotation;
+	private Vector3 _centerTargetRotation;
+	
+	private Vector3 _centerAimCurrentRotation;
+	private Vector3 _centerAimTargetRotation;
+	
+
+	private Vector3 _rotationSpeed;
+	private Vector3 _rotationAimSpeed;
+
+	private void HandleCamera()
+	{
+		if (Input.GetKey((KeyCode) Stering.Keys.Left))
+		{
+			_centerTargetRotation.y = _rotationFactor * _rotationSpeedX;
+		}
+		if (Input.GetKey((KeyCode) Stering.Keys.Right))
+		{
+			_centerTargetRotation.y = -_rotationFactor * _rotationSpeedX;
+		}
+		if (Input.GetKey((KeyCode) Stering.Keys.Up))
+		{
+			_centerAimTargetRotation.x = _rotationFactor * _rotationSpeedY;
+		}
+		if (Input.GetKey((KeyCode) Stering.Keys.Down))
+		{
+			_centerAimTargetRotation.x = -_rotationFactor * _rotationSpeedY;
+		}
+
+		_centerCurrentRotation = Vector3.SmoothDamp(_centerCurrentRotation, _centerTargetRotation, ref _rotationSpeed,
+			Time.smoothDeltaTime);
+		_centerAimCurrentRotation = Vector3.SmoothDamp(_centerAimCurrentRotation, _centerAimTargetRotation,
+			ref _rotationAimSpeed, Time.smoothDeltaTime);
+
+
+		_aimPoint.transform.Rotate(_centerAimCurrentRotation);
+		_mainCameraCenter.transform.Rotate(_centerCurrentRotation);
+
+		if (_centerCurrentRotation.AlmostEquals(_centerTargetRotation, 1f))
+		{
+			_centerCurrentRotation = Vector3.zero;
+			_centerTargetRotation = Vector3.zero;
+		}
+
+		if (_centerAimCurrentRotation.AlmostEquals(_centerAimTargetRotation, 1f))
+		{
+			_centerAimCurrentRotation = Vector3.zero;
+			_centerAimTargetRotation = Vector3.zero;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		HandleCamera();
+	}
+}
--- Assets/[Scripts]/MainController.cs
+++ Assets/[Scripts]/MainController.cs
@@ -7,31 +7,16 @@ public class MainController : MonoBehaviour
 {
 
 	public static Action<int> NextTurn;
-	public float _rotationFactor = 1f;
 
 	[SerializeField] 
 	private float _turnTime = 1f;
-	[SerializeField]
-	private GameObject _mainCameraCenter;
 	[SerializeField] 
-	private GameObject _aimPoint;
-	[SerializeField] 
-	private PhotonView myView;
+	private PhotonView  myView;
 
 	private float _lastTimeUpdate = Single.MinValue;
 	private int _turnCounter;
-	private float _rotationSpeedX = 1f;
-	private float _rotationSpeedY = 1f;
-	
-	private Vector3 _centerCurrentRotation;
-	private Vector3 _centerTargetRotation;
-	
-	private Vector3 _centerAimCurrentRotation;
-	private Vector3 _centerAimTargetRotation;
-	
 
-	private Vector3 _rotationSpeed;
-	private Vector3 _rotationAimSpeed;
+	private readonly string _mainPlayerPath = "Center";
 	
 
 	// Use this for initialization
@@ -39,60 +24,31 @@ public class MainController : MonoBehaviour
 	{
 		
 		NextTurn += HandleNextTurn;
+
+		SpownPlayer();
+	}
+
+	private void SpownPlayer()
+	{
+		Debug.Log("Spown");
+		if (PhotonNetwork.isMasterClient)
+		{
+			myView.RPC("CreatePlayer", PhotonTargets.All);
+		}
+		else if(!PhotonNetwork.connected)
+		{
+			Instantiate(Resources.Load(_mainPlayerPath));
+		}
 	}
 
 	private void HandleNextTurn(int TurnNumber)
 	{
-		//HandleCamera();
-	}
-
-	private void HandleCamera()
-	{
-		if (Input.GetKey((KeyCode)Stering.Keys.Left))
-		{
-			_centerTargetRotation.y = _rotationFactor * _rotationSpeedX;
-		}
-		if (Input.GetKey((KeyCode)Stering.Keys.Right))
-		{
-			_centerTargetRotation.y = -_rotationFactor * _rotationSpeedX;
-		}
-		if (Input.GetKey((KeyCode)Stering.Keys.Up))
-		{
-			_centerAimTargetRotation.x = _rotationFactor * _rotationSpeedY;
-        }
-		if (Input.GetKey((KeyCode)Stering.Keys.Down))
-		{
-			_centerAimTargetRotation.x = -_rotationFactor * _rotationSpeedY;
-        }
-
-		_centerCurrentRotation	= Vector3.SmoothDamp(_centerCurrentRotation, _centerTargetRotation, ref _rotationSpeed, Time.smoothDeltaTime);
-		_centerAimCurrentRotation = Vector3.SmoothDamp(_centerAimCurrentRotation, _centerAimTargetRotation,
-			ref _rotationAimSpeed, Time.smoothDeltaTime);
-		
-		
-		_aimPoint.transform.Rotate(_centerAimCurrentRotation);
-		_mainCameraCenter.transform.Rotate(_centerCurrentRotation);
-
-		if (_centerCurrentRotation.AlmostEquals(_centerTargetRotation, 1f))
-		{
-			_centerCurrentRotation = Vector3.zero;
-			_centerTargetRotation = Vector3.zero;
-		}
-		
-		if (_centerAimCurrentRotation.AlmostEquals(_centerAimTargetRotation, 1f))
-		{
-			_centerAimCurrentRotation = Vector3.zero;
-			_centerAimTargetRotation = Vector3.zero;
-		}
 		
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		
-		HandleCamera();
-
 		if (PhotonNetwork.isMasterClient)
 		{
 			if (Time.realtimeSinceStartup > _lastTimeUpdate + _turnTime)
@@ -104,5 +60,11 @@ public class MainController : MonoBehaviour
 				_lastTimeUpdate = Time.realtimeSinceStartup;
 			}
 		}
+	}
+
+	[PunRPC]
+	private void CreatePlayer()
+	{
+		PhotonNetwork.Instantiate(_mainPlayerPath, Vector3.zero, Quaternion.identity, 0);
 	}
 }
